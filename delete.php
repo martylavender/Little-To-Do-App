3d24840d8d58dcf2de0f6a0e307152556eac9fb7
Marty Lavender <martylavender@me.com>
2021-02-11 10:54:37 -0600

    Update delete.php
    Cleaned up code formatting

--- delete.php
+++ delete.php
@@ -1,26 +1,27 @@
 <?php
-
 session_start();
 
-if(!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']))
+{
     header("Location: index.php");
 }
 
 /**Include our MySQL connection.*/
 require 'database.php';
 
-try {
+try
+{
     $del_id = $_GET['del_id'];
 
     $delete = "DELETE userlists, subtasks FROM userlists INNER JOIN subtasks ON userlists.listID = subtasks.parentID WHERE listID = :del_id";
     $delete = $pdo->prepare($delete);
-    $delete->bindParam(':del_id',$_GET['del_id']);
+    $delete->bindParam(':del_id', $_GET['del_id']);
     $delete->execute();
     header("location:loggedin.php");
 }
 catch(PDOException $e)
 {
-    echo "error:".$e->getMessage();
+    echo "error:" . $e->getMessage();
 }
 
 ?>
