7013e6e09656e20f904a0a476a60fe3bbc94a163
Marty Lavender <martylavender@me.com>
2021-02-11 10:53:48 -0600

    Update deleteitem.php
    Cleaned up code formatting

--- deleteitem.php
+++ deleteitem.php
@@ -1,8 +1,8 @@
 <?php
-
 session_start();
 
-if(!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']))
+{
     header("Location: index.php");
 }
 
@@ -12,21 +12,24 @@ require 'database.php';
 $listID = $_GET['listID'];
 $parentID = $_GET['parentID'];
 
-try {
-        $delete = "DELETE FROM subtasks 
+try
+{
+    $delete = "DELETE FROM subtasks 
                    WHERE subtask_id = :listID 
                    AND parentID = :parentID";
-        $delete = $pdo->prepare($delete);
-        $delete->bindParam(':listID',$listID);
-        $delete->bindParam(':parentID',$parentID);
-        $delete->execute();
+    $delete = $pdo->prepare($delete);
+    $delete->bindParam(':listID', $listID);
+    $delete->bindParam(':parentID', $parentID);
+    $delete->execute();
 
-        $listID = $parentID;
+    $listID = $parentID;
 
-        header("location:list.php?listID=$listID&parentID=$parentID");
-        echo 'The delete happened';
+    header("location:list.php?listID=$listID&parentID=$parentID");
+    echo 'The delete happened';
     /* catch any exceptions that may occur */
-} catch(PDOException $e) {
-    echo "error:".$e->getMessage();
+}
+catch(PDOException $e)
+{
+    echo "error:" . $e->getMessage();
 }
 ?>
